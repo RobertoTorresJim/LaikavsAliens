79bff710f5fc23e5fd94e4561247afe5b7e196c7
Roberto Torres Jiménez <iruga_cross@hotmail.com>
2015-08-01 18:53:30 -0500

    Primer commit de peoyecto laika vs aliens

---
+++ LaikavsAliens/Assets/Scripts/Lk_Control.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Lk_Control : MonoBehaviour {
+	public float speed;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb.AddForce (movement * speed);
+
+	}
+}
